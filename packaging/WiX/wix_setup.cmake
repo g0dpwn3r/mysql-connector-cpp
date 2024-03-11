9f2c0a34c9e032e2aa2e248e0368ed80b3bf11f9
Rafał Somla <rafal.somla@oracle.com>
2024-03-11 09:42:01 +0100

    packaging/WiX: Add check for required WiX extensions

--- packaging/WiX/wix_setup.cmake
+++ packaging/WiX/wix_setup.cmake
@@ -45,3 +45,22 @@ ELSE ()
   ENDIF ($ENV{WIX_DIR})
 ENDIF ()
 
+
+# Check WIX extensions, our MSI build requires UI and Util from WixToolset.
+
+message(STATUS "==Installed WiX extensions")
+execute_process(
+  COMMAND ${WIX_EXECUTABLE} extension list
+  OUTPUT_VARIABLE wix_extension
+)
+message(STATUS "==========================")
+
+if(
+  NOT wix_extensions MATCHES "WixToolset.UI.wixext"
+  OR NOT wix_extensions MATCHES "WixToolset.Util.wixext"
+)
+  message(SEND_ERROR
+    "Required WiX extensions not detected (WixToolset.UI/Util)"
+  )
+endif()
+
